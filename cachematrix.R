3127a927783be52cdf6ed262b5b6ff7948722567
xlemontzoglou <xanthippi.lemontzoglou@hotmail.com>
2016-03-30 18:00:41 +0300

    ##creates a "special" vector.. a list of objects (reference Object Oriented Programming) containing function objects and variables that are saved in cache memory through <<- in order to "use" them through lexical scoping makeCacheMatrix<-function(M=numeric()) {     InM<-NULL     set<-function(y){         M<<-y         InM<<-NULL     }     get<-function() M     setInv<-function(solve) InM<<-solve     getInv<-function() InM     list(set=set, get=get,setInv=setInv,getInv=getInv) }
    ##function that calculates inverse matrix
    cacheSolve<-function(M,..)
    {
        InM<-M$getInv()
        if(!is.null(InM) ){
            message("Already calculated and in cache memory:")
            return(InM)
        }
        data<-M$get()
        InM<-solve(data)
        M$setInv(InM)
        InM
    }
    
    ##example of use of functions - x inversable matrix
    # x <- matrix( c(0,1,1,0), nrow=2, ncol=2)
    # cache_matrix <- makeCacheMatrix(x)
    # cacheSolve(cache_matrix)
    ##2nd call to verify that if already calculated it prints out the corresponding message and gives cached value
    # cacheSolve(cache_matrix)

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+##creates a "special" vector.. a list of objects (reference Object Oriented Programming) containing function objects and variables that are saved in cache memory through <<- in order to "use" them through lexical scoping
+makeCacheMatrix<-function(M=numeric())
+{
+    InM<-NULL                           
+    set<-function(y){                   
+        M<<-y                             
+        InM<<-NULL                        
+    }
+    get<-function() M                   
+    setInv<-function(solve) InM<<-solve     
+    getInv<-function() InM              
+    list(set=set, get=get,setInv=setInv,getInv=getInv)
+}  
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+##function that calculates inverse matrix
+cacheSolve<-function(M,..)           
+{
+    InM<-M$getInv()                    
+    if(!is.null(InM) ){                
+        message("Already calculated and in cache memory:")
+        return(InM)
+    }
+    data<-M$get()                      
+    InM<-solve(data)
+    M$setInv(InM)                     
+    InM
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+##example of use of functions - x inversable matrix
+# x <- matrix( c(0,1,1,0), nrow=2, ncol=2)
+# cache_matrix <- makeCacheMatrix(x)
+# cacheSolve(cache_matrix)
+##2nd call to verify that if already calculated it prints out the corresponding message and gives cached value
+# cacheSolve(cache_matrix)
